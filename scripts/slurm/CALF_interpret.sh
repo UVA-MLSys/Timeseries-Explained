eaa611d08f12641988087d284cdfb326d446c584
khairulislam <khairulislamtanim@gmail.com>
2024-12-04 08:18:20 -0500

    add calf extremal mask

--- scripts/slurm/CALF_interpret.sh
+++ scripts/slurm/CALF_interpret.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#SBATCH --job-name="mimic_interpret"
-#---SBATCH --output=results/outputs/CALF_traffic_interpret.out
+#SBATCH --job-name="calf_interpret"
+#---SBATCH --output=results/outputs/CALF_interpret.out
 #SBATCH --partition=gpu
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
@@ -11,30 +11,30 @@
 source /etc/profile.d/modules.sh
 source ~/.bashrc
 
-module load cuda-toolkit cudnn-8.9.5_cuda12.x anaconda3
+module load miniforge
 
-conda deactivate
 conda activate ml
 
+explainers=("feature_ablation" "occlusion" "augmented_occlusion" "feature_permutation" "integrated_gradients" "gradient_shap" "dyna_mask" "extremal_mask" "winIT" "wtsr" "gatemask" "tsr")
 function interpret {
   echo "Running $1-th job"
 
-  # python interpret_CALF.py\
-  #   --task_name long_term_forecast \
-  #   --explainers feature_ablation occlusion augmented_occlusion feature_permutation integrated_gradients gradient_shap winIT gatemask wtsr\
-  #   --root_path ./dataset/electricity/ \
-  #   --data_path electricity.csv \
-  #   --model CALF \
-  #   --features S \
-  #   --seq_len 96 \
-  #   --label_len 12 \
-  #   --pred_len 24 \
-  #   --n_features 1 --d_model 768\
-  #   --disable_progress
+  python interpret_CALF.py\
+    --task_name long_term_forecast \
+    --explainers $explainers\
+    --root_path ./dataset/electricity/ \
+    --data_path electricity.csv \
+    --model CALF \
+    --features S \
+    --seq_len 96 \
+    --label_len 12 \
+    --pred_len 24 \
+    --n_features 1 --d_model 768\
+    --disable_progress --itr_no $1
 
   python interpret_CALF.py\
     --task_name long_term_forecast \
-    --explainers feature_ablation occlusion augmented_occlusion feature_permutation winIT gatemask wtsr\
+    --explainers $explainers\
     --root_path ./dataset/traffic/ \
     --data_path traffic.csv \
     --model CALF \
@@ -45,16 +45,16 @@ function interpret {
     --n_features 1 --d_model 768\
     --disable_progress --itr_no $1
 
-  # python interpret_CALF.py \
-  #   --explainers feature_ablation occlusion augmented_occlusion feature_permutation integrated_gradients gradient_shap winIT gatemask wtsr\
-  #   --task_name classification \
-  #   --data mimic \
-  #   --root_path ./dataset/mimic_iii/ \
-  #   --data_path mimic_iii.pkl \
-  #   --metrics auc accuracy cross_entropy \
-  #   --model CALF --n_features 31 --seq_len 48\
-  #   --d_model 768 --overwrite \
-  #   --disable_progress --batch_size 16
+  python interpret_CALF.py \
+    --explainers $explainers\
+    --task_name classification \
+    --data mimic \
+    --root_path ./dataset/mimic_iii/ \
+    --data_path mimic_iii.pkl \
+    --metrics auc accuracy cross_entropy \
+    --model CALF --n_features 31 --seq_len 48\
+    --d_model 768 --overwrite \
+    --disable_progress --itr_no $1
 }
 
 interpret $SLURM_ARRAY_TASK_ID
